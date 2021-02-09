c675d6ac5acb7d9c7f7118bcc099e5731565a290
Austin Landis <austinlandiscode@gmail.com>
2021-02-09 13:44:05 -0800

    refactor UI logic.

--- src/main.js
+++ src/main.js
@@ -4,24 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GiphySearch from './giphy.js';
 
-$(document).ready(function() {
-  $('#searchBarArea').click(function(event) {
-    const userSearch = $('#userSearch').val();
-    event.preventDefault();
-
-    $('#userSearch').val("");
-
-    function getElements(response) {
-      let imgLink = `${response.data[0].images.downsized.url}`;
-      let imgLink1 = `${response.data[1].images.downsized.url}`;
-      let imgLink2 = `${response.data[2].images.downsized.url}`;
-      let trending1 = `${response.data[0].images.downsized.url}`;
-      
-      $('#showGiphs').html("<img src=\"" + imgLink + "\">");
-      $('#showGiphs1').html("<img src=\"" + imgLink1 + "\">");
-      $('#showGiphs2').html("<img src=\"" + imgLink2 + "\">");
-      $('#showTrending').html("<img src=\"" + trending1 + "\">");
-    }
+$('#searchBarArea').click(function(event) {
+  event.preventDefault();
+  const userSearch = $('#userSearch').val();
+  $('#userSearch').val("");
+  let promise = GiphySearch.findGiph(userSearch);
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    $('#showGiphs').html(`<img src="${body.data[0].images.downsized.url}">`);
+    $('#showGiphs1').html(`<img src="${body.data[1].images.downsized.url}">`);
+    $('#showGiphs2').html(`<img src="${body.data[2].images.downsized.url}">`);
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error}`);
   });
 });
-I
